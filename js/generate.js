7ac677e091d608e11bdb8e04fa66425ee24452fa
alanzhao1 <azhao101@gmail.com>
2015-02-09 03:24:33 -0500

    Moved scripts to appropriate position, initalized variables, and fixed annoying syntax bug

--- js/generate.js
+++ js/generate.js
@@ -1,6 +1,6 @@
 function generate() {
 	adj_index = Math.floor(Math.random() * adjs.length);
 	noun_index = Math.floor(Math.random() * nouns.length);
-
-	document.getElementById("output").innerHTML = adjs[adj_index} + " " + nouns[noun_index];
+	
+	document.getElementById("output").innerHTML = adjs[adj_index] + " " + nouns[noun_index];
 }

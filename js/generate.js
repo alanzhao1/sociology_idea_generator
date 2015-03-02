c6b505713b8fb6a7d237cbf8f8aa05561c4959b7
Alan Zhao <ayz2102@columbia.edu>
2015-03-02 01:06:26 +0000

    Improved code to center button, cleaned up javascript

--- js/generate.js
+++ js/generate.js
@@ -1,6 +1,6 @@
 function generate() {
-	noundes_index = Math.floor(Math.random() * noundescriptors.length);
-	noun_index = Math.floor(Math.random() * nouns.length);
+	var noundes_index = Math.floor(Math.random() * noundescriptors.length);
+	var noun_index = Math.floor(Math.random() * nouns.length);
 	
 	document.getElementById("output").innerHTML = noundescriptors[noundes_index] + " " + nouns[noun_index];
 }
